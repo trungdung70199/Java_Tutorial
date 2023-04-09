fcfd6e3bbad044df2c14054456be4b39ef9d778a
trungdung70199 <trungdung70199@gmail.com>
2023-04-09 22:27:45 +0700

    First program in Java

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("I am beginner Java");
+        System.out.print("Hello World \n");
+        System.out.print("with Java \n");
+        // Output Numbers
+        System.out.println("Your favorite number:");
+        System.out.println(7);
+    }
+}
+//NOTE: Java is case-sensitive: "Myclass" and "myclass"
+// has different meaning
+// The name of the java file must match the class name.
---
+++ Variables.java
@@ -0,0 +1,21 @@
+// Display Variables
+// Can also use the + character to add a variable to another variable
+// Declare Many Variables
+
+public class Variables {
+    public static void main(String[] args) {
+        String name = "Dug";
+        System.out.println("Hello " + name);
+
+        String firstname = "Dung. ";
+        String lastname = "Trung";
+        String fullname = firstname + lastname;
+        System.out.println(fullname);
+
+        //int variables
+        int xS = 5;
+        int y = 10;
+        int z = 15;
+        System.out.println(z + y + z);
+    }
+}
